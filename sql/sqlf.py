7eaf3fe8e0aaca27f748f9fccef96c4b83095a0f
RICKARD STEIEN <RICK6907@gmail.com>
2014-08-16 22:24:07 +0200

    assignment3

---
+++ sql/sqlf.py
@@ -0,0 +1,21 @@
+# HOINin data from multiple tables - cleanup
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+    c = connection.cursor()
+
+    # retrieve data
+    c.execute("""SELECT DISTINCT population.city, 
+              regions.region FROM population, regions
+              WHERE population.city = regions.city
+              ORDER by population.city ASC""")
+
+    rows = c.fetchall()
+
+    for r in rows:
+        print "City: " + r[0]
+        print "Population: " + str(r[1])
+        print "Region: " + r[2]
+        print
+
